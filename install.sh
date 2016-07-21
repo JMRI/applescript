f3774ccb8c626dd27071d7f5d7c1370ef0a885bb
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-07-21 11:48:33 +0000

    Creating site for 1.0

--- install.sh
+++ install.sh
@@ -6,13 +6,18 @@ LIB=${HOME}/Library/Application\ Support/JMRI/lib
 PREFS=${HOME}/Library/Preferences/JMRI
 mkdir -p "${LIB}"
 cd "${LIB}"
-curl -O https://github.com/rhwood/applescript/releases/download/v1.0/applescript-1.0.jar
+if [ ! -f "${LIB}/applescript-1.0.jar" ] ; then
+    curl -sSLO https://github.com/rhwood/applescript/releases/download/v1.0/applescript-1.0.jar
+fi
 cd "${PREFS}"
 if [ ! -f "${PREFS}/jmri.conf" ] ; then
     # download to avoid having to find a correct JMRI installation
-    curl -O https://raw.githubusercontent.com/JMRI/JMRI/master/jmri.conf
+    curl -sSLO https://raw.githubusercontent.com/JMRI/JMRI/master/jmri.conf
 fi
-if ! grep "^default_options" "${PREFS}/jmri.conf" | grep applescript-1.0.jar ; then
-    sed -i'' -e '/^default_options/s|"$| --cp:a=~/Library/Application\\ Support/JMRI/lib/applescript-1.0.jar"|' "${PREFS}/jmri.conf"
+if ! grep "^default_options" "${PREFS}/jmri.conf" | grep -q applescript-1.0.jar ; then
+    sed -i'' -e '/^default_options/s|"$| --cp:a=${HOME}/Library/Application\\ Support/JMRI/lib/applescript-1.0.jar"|' "${PREFS}/jmri.conf"
+    echo "AppleScript supported added to JMRI."
+else
+    echo "AppleScript already supported in JMRI."
 fi
 sed -i'' -e 's/^default_options=" /default_options="/' "${PREFS}/jmri.conf"
